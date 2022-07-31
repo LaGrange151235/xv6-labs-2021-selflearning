3fc58c2f68007e5227c112b91d72c2f18aa2bf4d
Jiayi Zhang <zhangjiayi1512@outlook.com>
2022-07-31 18:50:23 +0800

    Lab6 Multithreading

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -40,7 +40,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +54,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -105,6 +105,11 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  pthread_mutex_init(&lock[0], NULL);
+  pthread_mutex_init(&lock[1], NULL);
+  pthread_mutex_init(&lock[2], NULL);
+  pthread_mutex_init(&lock[3], NULL);
+  pthread_mutex_init(&lock[4], NULL);
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
